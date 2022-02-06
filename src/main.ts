38a38603f4c24dec71f743b4974872f4e11b78d0
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-02-06 15:37:10 +0100

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost*', '192.168.0.*'],
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
